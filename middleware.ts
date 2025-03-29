3a357ab41f169956d2618ec364c0a2a8f06a0f45
yoh <yokeus2000@gmail.com>
2025-03-29 19:15:19 +0900

    👷 Update env file

--- middleware.ts
+++ middleware.ts
@@ -6,35 +6,34 @@ const authRoutes = ["/sign-in", "/sign-up"];
 const passwordRoutes = ["/reset-password", "/forgot-password"];
 
 export default async function authMiddleware(request: NextRequest) {
-  const pathName = request.nextUrl.pathname;
-  const isAuthRoute = authRoutes.includes(pathName);
-  const isPasswordRoute = passwordRoutes.includes(pathName);
+	const pathName = request.nextUrl.pathname;
+	const isAuthRoute = authRoutes.includes(pathName);
+	const isPasswordRoute = passwordRoutes.includes(pathName);
 
-  const { data: session } = await betterFetch<Session>(
-    "/api/auth/get-session",
-    {
-      baseURL: process.env.BETTER_AUTH_URL,
-      headers: {
-        //get the cookie from the request
-        cookie: request.headers.get("cookie") || "",
-      },
-    },
-  );
+	const { data: session } = await betterFetch<Session>(
+		"/api/auth/get-session",
+		{
+			baseURL: process.env.BETTER_AUTH_URL,
+			headers: {
+				cookie: request.headers.get("cookie") || "",
+			},
+		},
+	);
 
-  if (!session) {
-    if (isAuthRoute || isPasswordRoute) {
-      return NextResponse.next();
-    }
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
+	if (!session) {
+		if (isAuthRoute || isPasswordRoute) {
+			return NextResponse.next();
+		}
+		return NextResponse.redirect(new URL("/sign-in", request.url));
+	}
 
-  if (isAuthRoute || isPasswordRoute) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+	if (isAuthRoute || isPasswordRoute) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
 
-  return NextResponse.next();
+	return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+	matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)", "/dashboard"],
 };
